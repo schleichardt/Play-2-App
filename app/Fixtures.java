54bb9c8da592fcf28b102e50f5554d40d52a2083
Michael Schleichardt <michael@schleichardt.net>
2012-05-30 22:40:32 +0200

    fixtures with deleteall for H2 Database

--- app/Fixtures.java
+++ app/Fixtures.java
@@ -1,21 +1,35 @@
 import com.avaje.ebean.Ebean;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
-import models.User;
+import org.apache.commons.dbutils.DbUtils;
 import play.Logger;
+import play.db.DB;
 import play.libs.Yaml;
 
 public class Fixtures {
     public static void loadAll() {
-        if(Ebean.find(User.class).findRowCount() == 0) {
-            final String fixtureFile = "initial-data.yml";
-            @SuppressWarnings("unchecked")
-            Map<String, List<Object>> tableMap = (Map<String, List<Object>>) Yaml.load(fixtureFile);//yaml must be in conf folder?
+        final String fixtureFile = "initial-data.yml";
+        @SuppressWarnings("unchecked")
+        Map<String, List<Object>> tableMap = (Map<String, List<Object>>) Yaml.load(fixtureFile);//yaml must be in conf folder?
 
-            for (Map.Entry<String, List<Object>> tableEntry : tableMap.entrySet()) {
-                Ebean.save(tableEntry.getValue());
-                Logger.info("loaded " + tableEntry.getValue().size() + " " + tableEntry.getKey() + " from '" + fixtureFile + "' into the database");
-            }
+        for (Map.Entry<String, List<Object>> tableEntry : tableMap.entrySet()) {
+            Ebean.save(tableEntry.getValue());
+            Logger.info("loaded " + tableEntry.getValue().size() + " " + tableEntry.getKey() + " from '" + fixtureFile + "' into the database");
+        }
+    }
+
+    public static void deleteAll() {
+        try {
+            final Connection connection = DB.getConnection();
+            final Statement sm = connection.createStatement();
+            sm.execute("DROP ALL OBJECTS");
+            DbUtils.closeQuietly(sm);
+            DbUtils.closeQuietly(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,8 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
         "org.springframework.security" % "spring-security-core" % "3.1.0.RELEASE",
-        "org.springframework.security" % "spring-security-crypto" % "3.1.0.RELEASE"
+        "org.springframework.security" % "spring-security-crypto" % "3.1.0.RELEASE",
+        "commons-dbutils" % "commons-dbutils" % "1.4"
     )
 
     val secureModule = Project("secure", file("modules/secure"))
